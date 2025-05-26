d9406d456dafdac49ebbcfce5178045015c660f5
dhanush2004-1 <harrydhanush24@gmail.com>
2025-05-26 20:25:11 +0530

    Delete Module 3 Core Java Answers directory

--- Module 3 Core Java Answers/BytecodeExample.java
+++
@@ -1,8 +0,0 @@
-public class BytecodeExample {
-    public static void sayHello() {
-        System.out.println("Hello");
-    }
-    public static void main(String[] args) {
-        sayHello();
-    }
-}
--- Module 3 Core Java Answers/LambdaSort.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class LambdaSort {
-    public static void main(String[] args) {
-        List<String> list = Arrays.asList("banana", "apple", "cherry");
-        Collections.sort(list, (a, b) -> a.compareTo(b));
-        for (String s : list) System.out.println(s);
-    }
-}
